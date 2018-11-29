eaf1bd4c2e8a1e7cf161bee8ab158ebc04495e92
Łukasz Opasiak <lo@idesign.center>
2018-11-29 10:57:58 +0100

    Do not break lines with function definition

--- dbugdb/debug.go
+++ dbugdb/debug.go
@@ -20,16 +20,14 @@ func New(db DB, logger Logger) DB {
 
 // ExecContext executes a query without returning any rows.
 // The args are for any placeholder parameters in the query.
-func (db debug) ExecContext(ctx context.Context, query string,
-	args ...interface{}) (sql.Result, error) {
+func (db debug) ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
 	db.Debug(parseQuery(query, args...))
 	return db.db.ExecContext(ctx, query, args...)
 }
 
 // QueryContext executes a query that returns rows, typically a SELECT.
 // The args are for any placeholder parameters in the query.
-func (db debug) QueryContext(ctx context.Context, query string,
-	args ...interface{}) (*sql.Rows, error) {
+func (db debug) QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error) {
 	db.Debug(parseQuery(query, args...))
 	return db.db.QueryContext(ctx, query, args...)
 }
@@ -39,8 +37,7 @@ func (db debug) QueryContext(ctx context.Context, query string,
 // Errors are deferred until Row's Scan method is called.
 // If the query selects no rows, the *Row's Scan will return ErrNoRows.
 // Otherwise, the *Row's Scan scans the first selected row and discards the rest.
-func (db debug) QueryRowContext(ctx context.Context, query string,
-	args ...interface{}) *sql.Row {
+func (db debug) QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row {
 	db.Debug(parseQuery(query, args...))
 	return db.db.QueryRowContext(ctx, query, args...)
 }
